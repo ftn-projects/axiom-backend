5f2bba5f4e081a7568334cba3ffda1c19e7fa408
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2024-05-21 23:45:49 +0200

    Update scrapper and add loaded data

--- scrapper.py
+++ scrapper.py
@@ -1,89 +1,67 @@
 from urllib.request import urlopen
 import re
 
-def find_paragraphs(table_matches, html, paragraph_matches, paragraphs_in_tables_indices):
-    for table_match in table_matches:
-        table_start_index = html.find(table_match)
-        table_end_index = table_start_index + len(table_match)
-        for i, paragraph_match in enumerate(paragraph_matches):
-            paragraph_start_index = html.find(paragraph_match)
-            if paragraph_start_index >= table_start_index and paragraph_start_index < table_end_index:
-                paragraphs_in_tables_indices.add(i)
 
-def remove_html_tags_and_entities(text):
-    clean = re.compile('<.*?>')
-    text = re.sub(clean, '', text)
-    text = text.replace('&#160;', ' ') 
-    return text
+BASE_URL = 'https://regularshow.fandom.com'
+DATA_DIR = 'data/'
 
-def sanitize_filename(name):
-    return re.sub(r'[<>:"/\\|?*]', '', name)
 
-def print_transcript(title, paragraph_matches, paragraphs_in_tables_indices):
-    title = sanitize_filename(title)
-    f = open(title + ".txt", "w", encoding='utf-8')
+def get_page_html(page_url: str):
+    return urlopen(page_url).read().decode("utf-8") 
 
-    for i, match in enumerate(paragraph_matches):
-        if i not in paragraphs_in_tables_indices:
-            clean_text = remove_html_tags_and_entities(match)
-            f.write(clean_text)
 
-    f.close()
+def get_episode_links(contents_url: str) -> list:
+    html = get_page_html(contents_url)
+    pattern = re.compile(r'<a[^>]*?href="([^"]*?/Transcript)"[^>]*?>')
+    return [BASE_URL + link for link in pattern.findall(html)]
 
-def extract_all_pages():
-    url = "https://regularshow.fandom.com/wiki/Category:Transcripts"
 
-    page = urlopen(url)
-    html_bytes = page.read()
-    html = html_bytes.decode("utf-8")
+def get_episode_title(html) -> str:
+    span_title_pattern = re.compile(r'<span[^>]*class="mw-page-title-main"[^>]*>(.*?)</span>', re.DOTALL)
+    h1_title_pattern = re.compile(r'<h1[^>]*id="firstHeading"[^>]*>(.*?)</h1>', re.DOTALL) 
 
-    a_tag_pattern = re.compile(r'<a[^>]*?href="([^"]*?/Transcript)"[^>]*?>')
+    match = span_title_pattern.search(html)
+    if match:
+        title = match.group(1)[:-11]
+    else:
+        match = h1_title_pattern.search(html).group(1)
+        title = re.compile(r'"(.*?)"').findall(match)[0]
 
-    a_tag_matches = a_tag_pattern.findall(html)
+    return re.sub(r'[<>:"/\\|?*]', '', title)
 
-    links = []
 
-    for link in a_tag_matches:
-        links.append("https://regularshow.fandom.com" + link)
+def paragraphs_outside_tables(html: str) -> list:
+    paragraph_pattern = re.compile(r'<p>(.*?)</p>', re.DOTALL)
+    table_pattern = re.compile(r'<table.*?</table>', re.DOTALL)
 
-    return links
+    table_ranges = {(m.start(), m.end()) for m in table_pattern.finditer(html)}
+    return [p for p in paragraph_pattern.findall(html) if all(not (s <= html.find(p) < e) for s, e in table_ranges)]
+
+
+def cleanup_html(html) -> str:
+    return re.sub('<.*?>', '', html).replace('&#160;', ' ')
+
+
+def load_episode(episode_url: str) -> tuple:
+    html = get_page_html(episode_url)
+
+    paragraphs = paragraphs_outside_tables(html)
+    raw_transcript = ''.join(paragraphs)
+
+    return get_episode_title(html), cleanup_html(raw_transcript)
 
 
 def main():
-    links = extract_all_pages()
+    contents_page1 = BASE_URL + '/wiki/Category:Transcripts'
+    contents_page2 = contents_page1 + '?from=Space+Escape%2FTranscript'
+    links = get_episode_links(contents_page1) + get_episode_links(contents_page2)
 
-    for url in links:
-        page = urlopen(url)
-        html_bytes = page.read()
-        html = html_bytes.decode("utf-8")
+    for link in links:
+        title, transcript = load_episode(link)
 
-        paragraph_pattern = re.compile(r'<p>(.*?)</p>', re.DOTALL)
-        paragraph_matches = paragraph_pattern.findall(html)
+        with open(DATA_DIR + title + ".txt", "w", encoding='utf-8') as f:
+            f.write(transcript)
 
-        table_pattern = re.compile(r'<table.*?</table>', re.DOTALL)
-        table_matches = table_pattern.findall(html)
-
-        paragraphs_in_tables_indices = set()
-
-        title_pattern = re.compile(r'<span[^>]*class="mw-page-title-main"[^>]*>(.*?)</span>', re.DOTALL)
-        title_match = title_pattern.search(html)
-
-        if title_match:
-            page_title = title_match.group(1)
-            title = page_title[:-11]
-        else:
-            h1_pattern = re.compile(r'<h1[^>]*id="firstHeading"[^>]*>(.*?)</h1>', re.DOTALL)
-            h1_match = h1_pattern.search(html)
-
-            page_title = h1_match.group(1)
-
-            pattern = re.compile(r'"(.*?)"')
-    
-            title = pattern.findall(page_title)[0]
-
-
-        find_paragraphs(table_matches, html, paragraph_matches, paragraphs_in_tables_indices)
-        print_transcript(title, paragraph_matches, paragraphs_in_tables_indices)
 
 if __name__ == "__main__":
     main()
